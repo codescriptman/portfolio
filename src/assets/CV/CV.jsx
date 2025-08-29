e89c587281014edf5e92dbf801415654b1fda9a0
Azazello <donazazello1312@gmail.com>
2025-08-29 16:14:18 +0300

    CV

--- src/assets/CV/CV.jsx
+++ src/assets/CV/CV.jsx
@@ -16,7 +16,7 @@ const CV = (props) => (
           </span>
         </div>
       </a>
-      <a className={style.link} href="">
+      <a className={style.link} href="../../../public/img/cv/CV.pdf">
         <div className={style.div}>
           <p className={style.txt}>CV</p>{" "}
           <span>
